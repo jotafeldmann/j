64d88ab6376289aade487f4ed512a4f712a2f09f
jotafeldmann <eusoujf@hotmail>
2025-05-14 01:44:13 -0300

    fix command name

--- utils/is-url-back.sh
+++ utils/is-url-back.sh
@@ -1,12 +1,12 @@
-# . url-is-back.sh google.com
-# . url-is-back.sh google.com 1m
+# . is-url-back.sh google.com
+# . is-url-back.sh google.com 1m
 
 URL=$1
 DELAY=${2:-"1m"}
 
 while true; \
 do clear; \
-	echo URL IS BACK?; \
+	echo IS URL BACK?; \
 	echo "Last update $(date)"; \
 	echo $(curl -s -o /dev/null -LI -w "%{http_code}" $URL) | \
 	grep 000 && sleep $DELAY || \
