debf56aa443b3d30432ea7e7a36cfd0d935de9c7
jotafeldmann <eusoujf@hotmail>
2025-05-14 01:53:57 -0300

    add shortcuts concept to differ from utils

--- j.sh
+++ j.sh
@@ -4,8 +4,7 @@
 # add to ~/.bashrc
 # add these lines to the end of the file
 
-# source \
-# ~/workspace/config/j/j.sh \
+# source ~/workspace/config/j/j.sh \
 # ~/workspace/config/j \
 # ~/.bashrc \
 # ~/workspace/config/local/local.sh \
@@ -13,16 +12,21 @@
 # ~/workspace/config/temp \
 # ~/workspace
 
+# Parameters for source
 export J_PATH=$1
 export J_CURRENT_SHELL_FILE=$2
 export J_LOCAL=$3
 export J_LOCAL_PATH=$4
 export J_TEMP_DIR=$5
 export J_WORKSPACE=$6
-export J_UTILS=$J_PATH/utils
-export J_CONFIG=$J_PATH/config
-export J_SH_MAIN_FILE=$J_PATH/j.sh
 
+# Configs
+export J_SH_MAIN_FILE=$J_PATH/j.sh
+export J_CONFIG=$J_PATH/config
+export J_SHORTCUTS=$J_PATH/shortcuts
+export J_UTILS=$J_PATH/utils
+
+# Init
 source $J_CONFIG/_init_.sh
-source $J_UTILS/_init_.sh
+source $J_SHORTCUTS/_init_.sh
 source $J_LOCAL
---
+++ shortcuts/_init_.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+function j() {
+  . $J_SHORTCUTS/j_shortcuts.sh $J_SHORTCUTS/j_shortcuts.sh "$@"
+}
+alias j=j
+
+
+export J_UTIL_SAVE_PATH_FILE=$J_TEMP_DIR/save-path.txt
+function jsp() {
+  . $J_SHORTCUTS/save-path.sh $J_SHORTCUTS/save-path.sh $J_UTIL_SAVE_PATH_FILE "$@"
+}
+alias jsp=jsp
+
+export J_UTIL_GIT=$J_TEMP_DIR/save-git.txt
+function g() {
+  . $J_SHORTCUTS/github.sh $J_SHORTCUTS/github.sh $J_UTIL_GIT "$@"
+}
+alias g=g
+
+export J_UTIL_CONTEXT=$J_TEMP_DIR/context.sh
+function context() {
+  . $J_SHORTCUTS/context.sh $J_SHORTCUTS/context.sh $J_UTIL_CONTEXT "$@"
+}
+alias jc=context
