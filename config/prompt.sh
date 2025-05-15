84fd0928b60370ae74e8d0a3ed922c309314fd69
jotafeldmann <eusoujf@hotmail>
2025-05-15 15:52:28 -0300

    detach prompt, set branch to new line

---
+++ config/prompt.sh
@@ -0,0 +1,33 @@
+# Prompt colors
+COLOR_GIT_CLEAN='\[\033[1;30m\]'
+COLOR_GIT_MODIFIED='\[\033[0;33m\]'
+COLOR_GIT_STAGED='\[\033[0;36m\]'
+COLOR_RESET='\[\033[0m\]'
+
+# Prompt GIT template
+function git_prompt() {
+  if [ -e ".git" ]; then
+    branch_name=$(git symbolic-ref -q HEAD)
+    branch_name=${branch_name##refs/heads/}
+    branch_name=${branch_name:-HEAD}
+
+    echo -n "\n→ "
+
+    if [[ $(git status 2> /dev/null | tail -n1) = *"nothing to commit"* ]]; then
+      echo -n "$COLOR_GIT_CLEAN$branch_name$COLOR_RESET"
+    elif [[ $(git status 2> /dev/null | head -n5) = *"Changes to be committed"* ]]; then
+      echo -n "$COLOR_GIT_STAGED$branch_name$COLOR_RESET"
+    else
+      echo -n "$COLOR_GIT_MODIFIED$branch_name*$COLOR_RESET"
+    fi
+
+    echo -n " "
+  fi
+}
+
+# Prompt template
+# based upon https://gist.github.com/michaelneu/943693f46f7aa249fad2e6841cd918d5
+function prompt() {
+  PS1="\[\]\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\] $(git_prompt)\[\]\n"
+}
+PROMPT_COMMAND=prompt
